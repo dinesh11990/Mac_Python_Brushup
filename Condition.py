4c233af0eb34e17cfe568a78518b666c271a2da3
dinesh11990 <dinesh11990@gmail.com>
2023-08-25 14:13:13 +0200

    Exercise

---
+++ Condition.py
@@ -0,0 +1,12 @@
+if(True):
+    print("Yes")
+else:
+    print("No")
+
+#Comparison Operator: Operator which is used to compare two variables
+
+Rover = "Lost"
+if(Rover=="Found"):
+    print("Success")
+else:
+    print("Failed")
---
+++ Ex5.py
@@ -0,0 +1,8 @@
+#Get User Input for Variable Clinton, If Clinton==Died Print "Obama meets Hilary" Else "Öbama meets Clinton""
+
+
+Clinton=input("Clinton Died or Not ?")
+if(Clinton=="Died"):
+    print("Obama meets Hilary")
+else:
+    print("Obama meets Clinton")
